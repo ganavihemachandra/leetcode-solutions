52ad2275b7ced9b91db15896b9f6f1f9f1cfd2d8
Ganavi Hemachandra <ganavih2000@gmail.com>
2024-12-14 07:31:46 +0000

    [LeetCode Sync] Runtime - 306 ms (5.15%), Memory - 25.1 MB (28.80%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -2,28 +2,30 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        
         rows, cols = len(grid), len(grid[0])
-        path = set()
         q = deque()
+        path = set()
         islands = 0
-        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        directions = [[-1,0], [1, 0], [0, -1], [0, 1]]
+
         def bfs(r, c):
             q.append([r, c])
             path.add((r, c))
             while q:
-                row, col = q.popleft()
+                r, c = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
-                    if (nr in range(rows) and
-                        nc in range(cols) and
-                        (nr, nc) not in path and
-                        grid[nr][nc] == '1'):
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(rows) and nc in range(cols) and grid[nr][nc]=='1' and (nr, nc) not in path):
                         q.append([nr, nc])
                         path.add((nr, nc))
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in path:
-                    islands +=1
+                    islands += 1
                     bfs(r, c)
         return islands
+
+# Time Complexity - O(rows * columns)
+# Space Complexity - O(rows * columns)
         
