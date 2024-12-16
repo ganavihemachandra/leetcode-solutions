f7c1b424e2f6d3f2680aa989c708dc6e19d618ca
Ganavi Hemachandra <ganavih2000@gmail.com>
2024-12-16 23:05:53 +0000

    [LeetCode Sync] Runtime - 8 ms (92.31%), Memory - 20.1 MB (100.00%)

--- solutions/0049-group-anagrams/solution.py
+++ solutions/0049-group-anagrams/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashdict = defaultdict(list)
+        anagramMap = defaultdict(list)
 
-        for word in strs:
-            anagram_word = ''.join(sorted(word))
-            hashdict[anagram_word].append(word)
-        return list(hashdict.values())
-        
+        for i in strs:
+            anagram_word = ''.join(sorted(i))
+            anagramMap[anagram_word].append(i)
+        return list(anagramMap.values())
+
+# Time Complexity - O(N * LlogL), L is the length of string
+# Space Complexity - O(N*L)
