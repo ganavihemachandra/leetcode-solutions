d9de196180bdd7d900aec04076386c31cac45c65
Ganavi Hemachandra <ganavih2000@gmail.com>
2024-12-16 22:41:33 +0000

    [LeetCode Sync] Runtime - 15 ms (49.72%), Memory - 31.1 MB (89.30%)

--- solutions/0217-contains-duplicate/solution.py
+++ solutions/0217-contains-duplicate/solution.py
@@ -1,9 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
-        
+
         for n in nums:
             if n in hashset:
                 return True
             hashset.add(n)
         return False
+
+# Time Complexity - O(N)
+# Space Complexity - O(N)
