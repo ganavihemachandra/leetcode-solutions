3f7ac0ed21e8752014c190289b5d59125d900ae8
Ganavi Hemachandra <ganavih2000@gmail.com>
2024-12-17 22:02:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ solutions/0020-valid-parentheses/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {
+            ']' : '[', 
+            '}' : '{',
+            ')' : '('
+        }
+
+        for i in s:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
+
+# TC: O(N)
+# SC: O(N)
