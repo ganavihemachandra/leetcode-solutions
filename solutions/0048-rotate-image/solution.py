d14afcd281b007a85ecbe050d5c1ba370eac59af
Ganavi Hemachandra <ganavih2000@gmail.com>
2024-12-30 20:33:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- solutions/0048-rotate-image/solution.py
+++ solutions/0048-rotate-image/solution.py
@@ -3,16 +3,18 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        l, r = 0, len(matrix) - 1
+        l, r = 0, len(matrix)-1
 
         while l < r:
             for i in range(r - l):
                 top, bottom = l, r
 
-                topleft = matrix[top][l+i]
+                topLeft = matrix[top][l+i]
                 matrix[top][l+i] = matrix[bottom-i][l]
                 matrix[bottom-i][l] = matrix[bottom][r-i]
                 matrix[bottom][r-i] = matrix[top+i][r]
-                matrix[top+i][r] = topleft
+                matrix[top+i][r] = topLeft
             l += 1
             r -= 1
+
+
