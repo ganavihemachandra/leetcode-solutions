55f8aea5501205a9d0cc89033e00055c85b122af
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-01-04 23:59:05 +0000

    [LeetCode Sync] Runtime - 1833 ms (5.16%), Memory - 30.2 MB (72.77%)

--- solutions/3594-identify-the-largest-outlier-in-an-array/solution.py
+++ solutions/3594-identify-the-largest-outlier-in-an-array/solution.py
@@ -3,14 +3,13 @@ class Solution:
         outlier = float('-inf')
         total_sum = sum(nums)
         counts = Counter(nums)
-        res = 0
 
         for n in nums:
             total_sum -= n
             counts[n] -= 1
             if total_sum % 2 == 0 and counts[total_sum // 2] > 0:
                 outlier = max(outlier, n)
-            total_sum +=n
-            counts[n] +=1
+            total_sum += n
+            counts[n] += 1
         return outlier
         
