338d3fe273010f35edee1a7ede76f7a4c3576c88
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-02-22 19:13:43 +0000

    [LeetCode Sync] Runtime - 323 ms (5.15%), Memory - 25.5 MB (28.39%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -2,28 +2,33 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        
         rows, cols = len(grid), len(grid[0])
         path = set()
-        islands = 0
+        island = 0
         q = deque()
+        directions = [[-1, 0], [0, 1], [1, 0,], [0, -1]]
 
         def bfs(r, c):
             q.append([r, c])
             path.add((r, c))
             while q:
-                r, c = q.popleft()
-                directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if (nr in range(rows) and nc in range(cols) and (nr, nc) not in path and grid[nr][nc]== '1'):
-                        q.append([nr, nc])
-                        path.add((nr, nc))
+                for i in range(len(q)):
+                    r, c = q.popleft()
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if (nr in range(rows) and nc in range(cols) and grid[nr][nc] == '1' and (nr, nc) not in path):
+                            q.append([nr, nc])
+                            path.add((nr, nc))
+        
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == '1' and (r, c) not in path:
-                    islands +=1
+                    island += 1
                     bfs(r, c)
-        return islands
+        return island
 
 # TC: O(rows * cols)
 # SC: O(rows * cols)
+            
+        
