d7904a807554fbbd2956b0e5532f769d03569ce8
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-02-22 17:47:07 +0000

    [LeetCode Sync] Runtime - 76 ms (29.72%), Memory - 23.1 MB (100.00%)

--- solutions/1014-k-closest-points-to-origin/solution.py
+++ solutions/1014-k-closest-points-to-origin/solution.py
@@ -6,10 +6,13 @@ class Solution:
             distance = x**2 + y**2
             minHeap.append([distance, x, y])
         heapq.heapify(minHeap)
-        
         res = []
         for i in range(k):
             distance, x, y = heapq.heappop(minHeap)
             res.append([x, y])
         return res
 
+# Operation	Time Complexity	Space Complexity
+# Building the heap (heapify)	O(N)	O(N)
+# Popping k elements	O(k log N)	O(k)
+# Total	O(N + k log N)	O(N)
