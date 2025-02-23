8a3d0651a0a891df76b85c8595dc6990ff780c86
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-02-23 23:54:24 +0000

    [LeetCode Sync] Runtime - 110 ms (9.15%), Memory - 18.8 MB (100.00%)

--- solutions/0227-basic-calculator-ii/solution.py
+++ solutions/0227-basic-calculator-ii/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def calculate(self, s: str) -> int:
         i = 0
-        cur = prev = res = 0
-        cur_operation = "+"
+        prev = cur = res = 0
+        cur_operation = '+'
 
         while i < len(s):
             cur_char = s[i]
-
             if cur_char.isdigit():
                 while i < len(s) and s[i].isdigit():
-                    cur = (cur* 10) + int(s[i])
+                    cur = cur * 10 + int(s[i])
                     i += 1
                 i -= 1
 
@@ -28,8 +27,10 @@ class Solution:
                     res += int(prev/cur)
                     prev = int(prev/cur)
                 cur = 0
+
             elif cur_char != ' ':
                 cur_operation = cur_char
             i += 1
         return res
+
         
