10b47c49d743ec289bfad29e5faa867e8dbd8d18
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-03-04 01:30:10 +0000

    [LeetCode Sync] Runtime - 2 ms (28.95%), Memory - 18.2 MB (100.00%)

--- solutions/0091-decode-ways/solution.py
+++ solutions/0091-decode-ways/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        dp = {len(s) : 1}
+        dp = {len(s): 1}
 
         def dfs(i):
             if i in dp:
@@ -8,12 +8,10 @@ class Solution:
             if s[i] == '0':
                 return 0
             
-            res = dfs(i + 1)
-            if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i+1] in '0123456')):
+            res = dfs(i+1)
+            if ((i+1) < len(s) and (s[i] == '1' or (s[i]== '2' and s[i+1] in '0123456'))):
                 res += dfs(i+2)
             dp[i] = res
             return res
         return dfs(0)
-
-# TC: O(N)
-# SC: O(N)
+        
