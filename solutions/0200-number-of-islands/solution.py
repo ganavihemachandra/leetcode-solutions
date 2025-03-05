e852a7e867d2d6ebe86d46a5e5e5aa89e7d56ffd
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-03-05 22:50:21 +0000

    [LeetCode Sync] Runtime - 323 ms (5.15%), Memory - 25.4 MB (28.54%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -4,31 +4,26 @@ class Solution:
             return 0
         
         rows, cols = len(grid), len(grid[0])
-        path = set()
-        island = 0
+        visit = set()
         q = deque()
-        directions = [[-1, 0], [0, 1], [1, 0,], [0, -1]]
+        islands = 0
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
         def bfs(r, c):
-            q.append([r, c])
-            path.add((r, c))
+            q.append((r, c))
+            visit.add((r, c))
             while q:
-                for i in range(len(q)):
-                    r, c = q.popleft()
-                    for dr, dc in directions:
-                        nr, nc = r + dr, c + dc
-                        if (nr in range(rows) and nc in range(cols) and grid[nr][nc] == '1' and (nr, nc) not in path):
-                            q.append([nr, nc])
-                            path.add((nr, nc))
+                r, c = q.popleft()
+
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (nr in range(rows) and nc in range(cols) and grid[nr][nc] == '1' and (nr, nc) not in visit):
+                        q.append((nr, nc))
+                        visit.add((nr, nc))
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and (r, c) not in path:
-                    island += 1
+                if grid[r][c] == '1' and (r, c) not in visit:
+                    islands += 1
                     bfs(r, c)
-        return island
-
-# TC: O(rows * cols)
-# SC: O(rows * cols)
-            
-        
+        return islands
