76e3c829907858d8c2964116a79c1f5945b48f76
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-03-06 00:05:47 +0000

    [LeetCode Sync] Runtime - 8 ms (28.03%), Memory - 21.8 MB (99.90%)

--- solutions/0130-surrounded-regions/solution.py
+++ solutions/0130-surrounded-regions/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows, cols = len(board), len(board[0])
-        
+
         def capture(r, c):
             if (r < 0 or r == rows or c < 0 or c == cols or board[r][c] != 'O'):
                 return
             board[r][c] = 'T'
-            directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+            directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
             for dr, dc in directions:
-                capture(r+dr, c+dc)
+                capture(r+dr, c + dc)
         
-        # 1. capture unsurrounded regions (O--> T)
+        # 1. DFS capture unsurrounded regions (O--> T)
         for r in range(rows):
             for c in range(cols):
-                if (board[r][c] == 'O' and (r in [0, rows-1] or c in [0, cols-1])):
+                if board[r][c] == 'O' and (r in [0, rows-1] or c in [0, cols-1]):
                     capture(r, c)
         
         # 2. capture surrounded regions (O --> X)
@@ -27,4 +27,3 @@ class Solution:
             for c in range(cols):
                 if board[r][c] == 'T':
                     board[r][c] = 'O'
-        
