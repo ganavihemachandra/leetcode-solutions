b4bf7d678a95f1458bc1672a3832891ae0b58c2c
Ganavi Hemachandra <ganavih2000@gmail.com>
2025-12-09 05:39:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (100.00%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,12 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        numMap = {}
+        numMap = {} # val --> idx
 
         for i in range(len(nums)):
             second = target - nums[i]
             if second in numMap:
                 return [numMap[second], i]
             numMap[nums[i]] = i
-
-# TC: O(N)
-# SC: O(N)   
